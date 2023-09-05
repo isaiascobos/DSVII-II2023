1fea4918bd930ff10ebb1c231c12b9bf35a18686
Isaias Cobos <isaias.cobos@utp.ac.pa>
2023-09-04 21:14:18 -0500

    lab 16 desarrollado hasta function potencia

---
+++ lab1/lab16.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>Laboratorio 1.6</title>
+    </head>
+    <body>
+        <?php
+            define('TAM', 4);
+            function potencia ($v1, $v2)
+            {
+                $rdo=pow($v1, $v2)
+                return $rdo;
+            }
+        ?>
+    </body>
+</html>
