159e0a0ef54b86544f997d48751250c3a0c0e70c
Isaias Cobos <isaias.cobos@utp.ac.pa>
2023-09-05 15:03:43 -0500

    se finaliza el lab 1.6 y lab 1.7

--- lab1/lab16.php
+++ lab1/lab16.php
@@ -10,6 +10,15 @@
                 $rdo=pow($v1, $v2);
                 return $rdo;
             }
+            echo "<table border=1>";
+            for ($n1=1; $n1<=TAM; $n1++)
+            {
+                echo "<tr>";
+                for ($n2=1; $n2<=TAM; $n2++)
+                echo "<td>". potencia($n1,$n2)."</td>";
+                echo "</tr>";
+            }
+        echo "</table>";
         ?>
     </body>
 </html>
