f1a102e584d976ab3747807d8ff8734804f38c36
isaiascobos <112462232+isaiascobos@users.noreply.github.com>
2023-11-23 20:12:24 -0500

    archivos del lab 5

---
+++ lab5/lab52.php
@@ -0,0 +1,22 @@
+<?php
+if (is_uploaded_file ($_FILES['nombre_archivo_cliente']['tmp_name'])) {
+    $nombreDirectorio = "archivos/";
+    $formatoArchivo = array ('jpg', 'jpeg', 'gif', 'png');
+    $nombreArchivo = $_FILES['nombre_archivo_cliente']['name'];
+    $nombreCompleto = $nombreDirectorio . $nombreArchivo;
+    if (is_file($nombreCompleto)) {
+        $idUnico = time();
+        $nombreArchivo = $idUnico . "-" . $nombreArchivo;
+            echo "Archivo repetido, se cambiarà el nombre a $nombreArchivo <br><br>"
+    }
+
+
+
+    move_uploaded_file ($_FILES['nombre_archivo_cliente']['tmp_name'], $nombreDirectorio . $nombreArchivo);
+
+    echo "El archivo se ha subido satisfactoriamente al directorio $nombreDirectorio <br>";
+}
+
+else 
+    echo "No se ha podido subir el archivo <br>";
+?>
